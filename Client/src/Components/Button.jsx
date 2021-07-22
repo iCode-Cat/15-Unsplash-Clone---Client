79aa9042b1d34347c6f3adc7a76e4d4e13a00d84
iCode-Cat <74076633+iCode-Cat@users.noreply.github.com>
2021-07-23 00:15:16 +0300

    Delete Client directory

--- Client/src/Components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Scss/Button.scss';
-
-const Button = ({ text, type, size }) => {
-  return <button className={`btn ${type}  ${size}`}>{text}</button>;
-};
-
-export default Button;
--- Client/src/Components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loader = () => {
-  return <div style={{ textAlign: 'center' }}>LOADING...</div>;
-};
-
-export default Loader;
